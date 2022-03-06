c04374412e1cec714d53a04a99b956c9f201bcb1
Jiri Prajz <jiri.prajz@advantages.cz>
2022-03-06 23:04:55 +0100

    add source code of plugin.

---
+++ plugin/plugin-fileupload-section/src/index.tsx
@@ -0,0 +1,129 @@
+import S from './styles.module.scss';
+import { observable } from "mobx";
+import React, { useState } from "react";
+import moment from "moment";
+import {
+  ILocalization,
+  ILocalizer,
+  IPluginData,
+  ISectionPlugin
+} from "@origam/plugin-interfaces";
+import { FilePond,registerPlugin } from "react-filepond";
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+
+import 'filepond/dist/filepond.min.css'
+
+
+// Import the Image EXIF Orientation and Image Preview plugins
+// Note: These need to be installed separately
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+
+// Register the plugins
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+
+// Register the plugin
+registerPlugin(FilePondPluginFileValidateType);
+
+
+const apiurl = "ApiUrl";
+const filterFileType = "FilterFileType";
+const invalidFileTypeMessage = "InvalidFileTypeMessage"
+const maxParallelUploads = "MaxParallelUploads"
+const instantUpload = "InstantUpload"
+
+export class FileUploadSectionPlugin implements ISectionPlugin {
+  requestSessionRefresh: (() => Promise<any>) | undefined;
+  setScreenParameters: ((parameters: { [key: string]: string; }) => void) | undefined;
+  $type_ISectionPlugin: 1 = 1;
+  id: string = ""
+  apiurl: string ;
+  filterFileType: string | undefined;
+  invalidFileTypeMessage: string | undefined;
+  instantUpload:boolean | undefined;
+  maxParallelUploads:number | undefined;
+
+  @observable
+  initialized = false;
+
+  initialize(xmlAttributes: { [key: string]: string }): void {
+    this.apiurl = this.getXmlParameter(xmlAttributes, apiurl);
+    this.filterFileType = this.getXmlParameter(xmlAttributes, filterFileType);
+    this.invalidFileTypeMessage = this.getXmlParameter(xmlAttributes, invalidFileTypeMessage);
+    this.instantUpload = (this.getXmlParameter(xmlAttributes, instantUpload) =="true");
+    this.maxParallelUploads = Number.parseInt(this.getXmlParameter(xmlAttributes, maxParallelUploads));
+    this.initialized = true;
+  }
+  getXmlParameter(xmlAttributes: { [key: string]: string }, parameterName: string) {
+    if (!xmlAttributes[parameterName]) {
+      throw new Error(`Parameter ${parameterName} was not found.`)
+    }
+    return xmlAttributes[parameterName];
+  }
+
+  getComponent(data: IPluginData, createLocalizer: (localizations: ILocalization[]) => ILocalizer): JSX.Element {
+    const localizer = createLocalizer([]);
+    
+    moment.locale(localizer.locale)
+    if (!this.initialized) {
+      return <></>;
+    }
+    console.info(data.dataView);
+    const refRowId = data.dataView.tableRows[0][1];
+    if(refRowId == null)
+    {
+      return <></>;
+    }
+    const entityId = data.dataView.tableRows[0][2];
+
+    const url = this.apiurl + "?refrowid=" + refRowId + "&entityid="+entityId;
+    console.info(url);
+    return (<FilePondComponent fileType={this.filterFileType} apiurl={url} invalidFileTypeMessage={this.invalidFileTypeMessage} 
+    instantUpload={this.instantUpload} maxParallelUploads={this.maxParallelUploads} />    );
+  }
+  @observable
+  getScreenParameters: (() => { [parameter: string]: string }) | undefined;
+
+  generateData( data: IPluginData, column: string) {
+    return data.dataView.tableRows
+       .map(row => 
+             data.dataView.getCellValue(row, column)
+         );
+   }
+}
+
+export const FilePondComponent: React.FC<{
+  fileType:string | undefined;
+  apiurl:string;
+  invalidFileTypeMessage:string | undefined
+  instantUpload:boolean | undefined
+  maxParallelUploads:number | undefined
+
+}> = (props) => {
+  const ftype: string = props.fileType ?? "";
+  const [files] = useState([])
+  const [setFiles]:any = useState([])
+  return (
+    <div className={S.mainContainer}>
+      <div className={S.subContainer}>
+      <div className="FilePondComponent" >
+           <FilePond
+              server={props.apiurl}
+              allowFileTypeValidation={true}
+              acceptedFileTypes={[ftype]}
+              labelFileTypeNotAllowed={props.invalidFileTypeMessage}
+              instantUpload={props.instantUpload??false}
+              maxParallelUploads={props.maxParallelUploads??1}
+              files={files}
+              allowReorder={true}
+              allowMultiple={true}
+              onupdatefiles={setFiles}
+              onerror={(error) => {if(error.code == 401) {alert("Please logout and login again.")} else {alert(error.body)}}}
+              labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+      />
+      </div>
+      </div>
+      </div>
+  )
+}
