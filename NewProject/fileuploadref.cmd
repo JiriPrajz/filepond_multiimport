e35376a13f66354cccfc2c27025b0fea3726ebdf
Jiri Prajz <jiri.prajz@advantages.cz>
2022-03-06 16:07:23 +0100

    model fileupload plugin with reference on parent r

--- NewProject/fileuploadref.cmd
+++ NewProject/fileuploadref.cmd
@@ -1,2 +1,2 @@
-docker run --env-file D:\OrigamProjects\fileuploadref\NewProject\fileuploadref.env -it -v D:\OrigamProjects\fileuploadref:/home/origam/HTML5/data/origam -p 8080:8080 origam/server:master-latest
+docker run --env-file D:\OrigamProjects\fileuploadref\NewProject\fileuploadref.env -it --name fileupload -v D:\OrigamProjects\fileuploadref:/home/origam/HTML5/data/origam -p 8080:8080 origam/server:master-latest
 
